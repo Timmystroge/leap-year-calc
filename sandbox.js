54abdffa3cedbaee87a95306734d354c05f467b2
TimmySTroge <Timmystroge75@gmail.com>
2024-03-09 22:44:11 +0100

    initial commit

---
+++ sandbox.js
@@ -0,0 +1,29 @@
+/* 
+Leap Year can be:
+    evenly divided by 4
+    cannot be evenly divided by 100
+    ecenly divided by 400
+*/
+
+const { log } = console;
+
+let userInput = 2000;
+
+const is_leap = (year) => {
+  if (year % 4 === 0) {
+    if (year % 100 === 0) {
+      if (year % 400 === 0) {
+        return true;
+      }
+      return false;
+    }
+    return true;
+  } else {
+    return false;
+  }
+};
+
+log(`${userInput} : ${is_leap(userInput)}`);
+
+// HAPPY CODING!
+// TIMMYSTROGE
